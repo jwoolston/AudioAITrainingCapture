e817f082717b933daa7e9b2e5b8aeb1e3c2fff0f
Jared Woolston <Jared.Woolston@gmail.com>
2023-07-20 21:29:14 -0700

    Working on threshold detection via RMS.

--- AudioHandler.py
+++ AudioHandler.py
@@ -1,26 +1,37 @@
+import math
+
+import librosa.onset
 import numpy as np
+import scipy.signal.windows
 from numpy.lib import stride_tricks
 
 import pyaudio
 from scipy import signal
-from scipy.fftpack import rfft
+from scipy.fftpack import fft
 
 
 class AudioHandler(object):
     def __init__(self):
         # pyaudio
+        self.threshold = None
+        self.rms = None
         self.spectrogram_t = None
         self.spectrogram_f = None
         self.spectrogram_A = None
-        self.wf_data = None
-        self.FORMAT = pyaudio.paInt16
+        self.FORMAT = pyaudio.paFloat32
         self.CHANNELS = 1
         self.RATE = 44100
         self.CHUNK = 1024 * 1
+        self.WINDOW_SIZE = 512
+        self.BUFFER_BLOCKS = int(math.ceil(self.RATE / 2 / self.CHUNK))
         self.p = None
         self.stream = None
-        self.traces = dict()
-        self.reference_db = 1  # 32768
+
+        # Create a buffer for storing 500ms worth of signal
+        self.data_buffer = np.zeros(self.BUFFER_BLOCKS * self.CHUNK)
+
+        self.ring_buffer_index = 0
+        self.ring_buffer_full = False
 
     def start(self):
         self.p = pyaudio.PyAudio()
@@ -37,15 +48,63 @@ class AudioHandler(object):
         self.p.terminate()
 
     def callback(self, in_data, frame_count, time_info, flag):
-        self.wf_data = np.frombuffer(in_data, dtype=np.int16)
-        self.spectrogram_f, self.spectrogram_t, self.spectrogram_A = signal.spectrogram(self.wf_data, self.RATE,
-                                                                                        detrend=False,
-                                                                                        mode='psd')
+        # Store the waveform data for this update
+        wf_data = np.frombuffer(in_data, dtype=np.float32)
+
+        if self.ring_buffer_full:
+            self.data_buffer = np.roll(self.data_buffer, -frame_count)
+            self.data_buffer[-frame_count::] = wf_data
+        else:
+            index = self.ring_buffer_index * frame_count
+            self.data_buffer[index:index + frame_count] = wf_data
+            self.ring_buffer_index += 1
+            if self.ring_buffer_index == self.BUFFER_BLOCKS:
+                # Buffer has filled, mark it full and collect a baseline of noise
+                self.ring_buffer_full = True
+                print(f'Ring buffer filled')
+                noise_median = np.percentile(self.rms[::], 50)
+                print(f'Noise Median: {noise_median}')
+                sigma = np.percentile(self.rms[::], 84.1) - noise_median
+                print(f'Noise Sigma: {sigma}')
+                # Set the minimum RMS energy threshold that is needed in order to declare
+                # an "onset" event to be equal to 5 sigma above the median
+                self.threshold = noise_median + 5 * sigma
+                print(f'RMS Threshold: {self.threshold}')
+
+        onsets = librosa.onset.onset_detect(y=self.data_buffer, sr=self.RATE, backtrack=True)
+
+        # Calculate RMS energy per frame.  I shortened the frame length from the
+        # default value in order to avoid ending up with too much smoothing
+        self.rms = librosa.feature.rms(y=self.data_buffer, frame_length=512)[0, ]
+
+        if self.threshold is not None:
+            thresh_idx = [self.rms > self.threshold]
+            print(f'Threshold Indices: {thresh_idx}')
+        # self.spectrogram_f, self.spectrogram_t, self.spectrogram_A = signal.spectrogram(self.data_buffer, self.RATE,
+        #                                                                                detrend=False,
+        #                                                                                mode='psd')
 
         return None, pyaudio.paContinue
 
+    # def determine_window_range(self):
+    #     # Total number of blocks in the ring buffer
+    #     max_count = self.ring_buffer_view.shape[0]
+    #
+    #     # Determine the number of filled blocks in the ring buffer
+    #     if self.ring_buffer_full:
+    #         count = max_count
+    #     else:
+    #         # The number of full blocks on the front side of the buffer
+    #         count = 0 if self.ring_buffer_index == 0 else self.ring_buffer_index - 1
+    #
+    #     # Determine indices. This should start at the next write position (oldest data) and go to the most recent block
+    #     return np.append(np.arange(self.ring_buffer_index, count), np.arange(self.ring_buffer_index))
+
     def get_latest_waveform(self):
-        return self.wf_data
+        return self.data_buffer
+
+    def get_latest_rms(self):
+        return self.rms
 
     def get_latest_spectrogram(self):
         return [self.spectrogram_t, self.spectrogram_f, self.spectrogram_A]
--- Gui.py
+++ Gui.py
@@ -1,12 +1,8 @@
 import PyQt5
-import pyqtgraph
-from PyQt5 import QtWidgets
 from PyQt5.QtCore import QRectF
 from pyqtgraph.Qt import QtCore
 import pyqtgraph as pg
 import numpy as np
-import matplotlib.pyplot as plt
-
 
 import sys
 
@@ -14,7 +10,7 @@ import sys
 class Gui(object):
     def __init__(self, audio_handler):
         self.audio_handler = audio_handler
-        self.SIZE_X = self.audio_handler.CHUNK
+        self.SIZE_X = self.audio_handler.CHUNK * self.audio_handler.BUFFER_BLOCKS
         self.FS = self.audio_handler.RATE
 
         self.app = pg.mkQApp("Hit Detect Training Capture")
@@ -25,11 +21,16 @@ class Gui(object):
         self.waveform = self.win.addPlot(
             title='WAVEFORM', row=1, col=1  # axisItems={'bottom': wf_xaxis},
         )
+        self.rms_plot = self.win.addPlot(
+            title='RMS', row=2, col=1
+        )
         # A plot area (ViewBox + axes) for displaying the image
         self.spectrogram = self.win.addPlot(
-            title='SPECTROGRAM', row=2, col=1
+            title='SPECTROGRAM', row=3, col=1
         )
 
+        self.rms_x = None
+        self.rms_trace = None
         self.waveform_x = np.arange(0, self.SIZE_X)
         self.waveform_trace = None
         self.spectrogram_trace = False
@@ -43,18 +44,27 @@ class Gui(object):
 
         self.spectrogram.setMouseEnabled(x=False, y=False)
         self.spectrogram.hideButtons()
-        self.spectrogram.addColorBar(self.spectrogram_img, values=(0, 30_000), label='Spectral Power', limits=(0, None), colorMap='inferno')
+        self.spectrogram.addColorBar(self.spectrogram_img, values=(0, 30_000), label='Spectral Power', limits=(0, None),
+                                     colorMap='inferno')
 
     def draw(self):
         waveform = self.audio_handler.get_latest_waveform()
+        rms = self.audio_handler.get_latest_rms()
         spectrogram = self.audio_handler.get_latest_spectrogram()
         if waveform is not None:
             self.update_waveform(waveform)
+        if rms is not None:
+            self.update_rms(rms)
         if spectrogram is not None:
             self.update_spectrogram(spectrogram)
 
     def update_waveform(self, waveform):
         self.set_waveform_data(waveform)
+
+    def update_rms(self, rms):
+        if self.rms_x is None:
+            self.rms_x = np.arange(0, len(rms))
+        self.set_rms_data(rms)
 
     def update_spectrogram(self, spectrogram):
         self.set_spectrogram_data(spectrogram[0], spectrogram[1], spectrogram[2])
@@ -72,9 +82,16 @@ class Gui(object):
     def set_waveform_data(self, data):
         if self.waveform_trace is None:
             self.waveform_trace = self.waveform.plot(pen='c', width=3)
-            self.waveform.setYRange(-32767, 32768, padding=0)
+            self.waveform.setYRange(-1, 1, padding=0)
             self.waveform.setXRange(0, self.SIZE_X, padding=0.005)
         self.waveform_trace.setData(self.waveform_x, data)
+
+    def set_rms_data(self, data):
+        if self.rms_trace is None:
+            self.rms_trace = self.rms_plot.plot(pen='r', width=3)
+            self.rms_plot.setYRange(0, 0.5, padding=0)
+            self.rms_plot.setXRange(0, len(self.rms_x), padding=0.005)
+        self.rms_trace.setData(self.rms_x, data)
 
     def set_spectrogram_data(self, time, freq, amplitude):
         if amplitude is None:
@@ -87,10 +104,7 @@ class Gui(object):
         rect = QRectF(0, 0, time[-1], freq[-1])
 
         self.spectrogram_img.setImage(amplitude, rect=rect)
-        #self.spectrogram_img.setLevels([0, 20000])
 
-        # Scale the X and Y Axis to time and frequency (standard is pixels)
-        # self.spectrogram.setYRange(0, freq[-1])
         # Limit panning/zooming to the spectrogram
         self.spectrogram.setLimits(xMin=0, xMax=time[-1], yMin=0, yMax=freq[-1])
         # Add labels to the axis
